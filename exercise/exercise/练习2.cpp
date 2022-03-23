3619392a4d48e810944eb5e18dbef8fe099a10a8
hjinhe <hjinhe@mai2.sysu.edu.cn>
2022-03-23 09:49:19 +0800

    练习

--- exercise/exercise/练习2.cpp
+++ exercise/exercise/练习2.cpp
@@ -2,9 +2,57 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+void menu()
+{
+
+	printf("######################################\n");
+	printf("#########1 猜数字      0 退出#########\n");
+	printf("######################################\n");
+}
+
+void game()
+{
+	int ret = 0;
+	int guess = 0;
+	ret = rand() % 100 + 1;
+	printf("请猜测一个 1 到 100 的整数\n");
+	while (1)
+	{
+		scanf("%d", &guess);
+		if (guess < ret)
+			printf("猜小了\n");
+		else if (guess > ret)
+			printf("猜大了\n");
+		else
+		{
+			printf("猜对了\n");
+			break;
+		}
+	}
+}
 int main()
 {
-	int a = 0;
-
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出程序\n");
+			break;
+		default:
+			printf("输入错误\n");
+			break;
+		}
+	} while (input);
 	return 0;
 }
